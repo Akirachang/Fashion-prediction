17168a260cd254bca8d055e897009ac8cd0dc754
林志儒 <linzr17@mails.tsinghua.edu.cn>
2020-07-15 14:05:24 +0800

    modify file output style only

--- LSTM/pre.py
+++ LSTM/pre.py
@@ -18,18 +18,20 @@ def get_picture():
     path = 'images/'
     dirs = os.listdir(path)
     count = 0
-    for row in range(load_csv.shape[0]):
+    file = open('only_style.txt', 'w')
+    for row in range(0, 10):
         tmp_line = {}
         img = str(load_csv.loc[row, 'id']) + '.jpg'
         if img in dirs:
-            img_path = path + img
-            tmp_line['id'] = load_csv.loc[row, 'id']
-            tmp_line['gender'] = load_csv.loc[row, 'gender']
-            tmp_line['year'] = load_csv.loc[row, 'year']
-            tmp_line['major_type'] = load_csv.loc[row, 'articleType']
-            tmp_line['major_color'] = load_csv.loc[row, 'baseColour']
+            # img_path = path + img
+            # tmp_line['id'] = load_csv.loc[row, 'id']
+            # tmp_line['gender'] = load_csv.loc[row, 'gender']
+            # tmp_line['year'] = load_csv.loc[row, 'year']
+            # tmp_line['major_type'] = load_csv.loc[row, 'articleType']
+            # tmp_line['major_color'] = load_csv.loc[row, 'baseColour']
             try:
                 tmp_line['style'] = get_feature('30bc8f97-7fac-4567-a287-b64acb990792', img_path)
+                file.write(str(tmp_line['style']) + '\n')
             except:
                 print('an error occured!')
             print("saved!")
@@ -41,6 +43,7 @@ def get_picture():
 
 if __name__ == '__main__':
     load_csv = pd.read_csv('styles.csv', error_bad_lines=False)
+    print(load_csv.shape[0])
     output = get_picture()
     output.to_csv('df.csv', index_label='index')
 
