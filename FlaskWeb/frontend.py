7b58f084a444adfc6893acb616b73b1d4eea9822
常晟 <changs17@mails.tsinghua.edu.cn>
2020-07-26 13:29:02 +0800

    flask

---
+++ FlaskWeb/frontend.py
@@ -0,0 +1,24 @@
+from flask import Flask, redirect, url_for, render_template, request
+
+app = Flask(__name__)
+
+# @app.route("/")
+# def home():
+#     return render_template("index.html")
+
+@app.route("/", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        color = request.form["color"]
+        types = request.form["type"]
+        style = request.form["style"]
+        print("颜色："+color)
+        print("款式："+types)
+        print("风格："+style)
+        #对接
+        return render_template("#292_folium_chloropleth_USA1.html")
+    else:
+        return render_template("login.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- LSTM/pre.py
+++ LSTM/pre.py
@@ -18,12 +18,12 @@ def get_picture():
     path = 'images/'
     dirs = os.listdir(path)
     count = 0
-    file = open('only_style.txt', 'w')
-    for row in range(0, 10):
+    file = open('37000-40000.txt', 'w')#1
+    for row in range(34000, 37000):#2
         tmp_line = {}
         img = str(load_csv.loc[row, 'id']) + '.jpg'
         if img in dirs:
-            # img_path = path + img
+            img_path = path + img
             # tmp_line['id'] = load_csv.loc[row, 'id']
             # tmp_line['gender'] = load_csv.loc[row, 'gender']
             # tmp_line['year'] = load_csv.loc[row, 'year']
@@ -33,6 +33,7 @@ def get_picture():
                 tmp_line['style'] = get_feature('30bc8f97-7fac-4567-a287-b64acb990792', img_path)
                 file.write(str(tmp_line['style']) + '\n')
             except:
+                file.write('\n')
                 print('an error occured!')
             print("saved!")
         df = df.append(tmp_line, ignore_index=True)
